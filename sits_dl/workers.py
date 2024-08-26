1cada324728c36f435386a499a9935f70e5a3f67
Florian Katerndahl <florian@katerndahl.com>
2024-08-26 12:59:59 +0000

    add docstring

--- sits_dl/workers.py
+++ sits_dl/workers.py
@@ -7,6 +7,27 @@ import numpy as np
 from sits_dl.forestmask import ForestMask
 
 def reader(tile_height: int, tile_width: int, n_bands: int, sequence: int, rstep: int, cstep:int, cube_inputs: List, forest_mask: ForestMask, q: pmp.Queue) -> None:
+    """Read Chunks of image and put them into a Queue
+
+    :param tile_height: Height of input image in pixel
+    :type tile_height: int
+    :param tile_width: Width of input image in pixel
+    :type tile_width: int
+    :param n_bands: Number of spectral bands of input image
+    :type n_bands: int
+    :param sequence: Sequence length of time series the used DL model can process
+    :type sequence: int
+    :param rstep: Row-wise step size used when looping over image
+    :type rstep: int
+    :param cstep: Column-wise step size used when looping over image
+    :type cstep: int
+    :param cube_inputs: List of input files for data cube granule
+    :type cube_inputs: List
+    :param forest_mask: Forest mask class used to construct mask layer
+    :type forest_mask: ForestMask
+    :param q: Queue into which read chunks are placed
+    :type q: pmp.Queue
+    """    
     for row in range(0, tile_height, rstep):
         for col in range(0, tile_width, cstep):
             mask: Optional[np.ndarray] = forest_mask.get_mask(row, row + rstep, col, col + cstep)
